34738bd33c0ded061928eb097fd4ce8ffb105f52
Deepak Chauhan <imdeepakchauhan12@gmail.com>
2024-12-24 00:23:25 +0530

    Ensure registration endpoint returns JSON response.
    Updated the `register` method in `AuthController` to send a JSON response instead of plain text upon successful user creation. Added a test case to verify the response type is `application/json`.

--- coverage/users/register.spec.ts
+++ coverage/users/register.spec.ts
@@ -14,6 +14,19 @@ describe('register user block - POST - /auth/register', () => {
             const response = await request(app).post('/auth/register').send(userData);
             expect(response.status).toBe(201);
         });
+
+        it('should return valid json', async () => {
+            const userData = {
+                firstName: 'John',
+                lastName: 'Doe',
+                email: 'johndoe@gmail.com',
+                password: 'password',
+            }
+
+            // eslint-disable-next-line @typescript-eslint/no-misused-promises
+            const response = await request(app).post('/auth/register').send(userData);
+            expect(response.type).toBe('application/json');
+        });
     });
     describe('Sad path', () => {
         it('should return status code 400', async () => {});
--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -2,6 +2,8 @@ import express from 'express';
 
 export class AuthController {
     register(req: express.Request, res: express.Response) {
-        res.status(201).send('User created');
+        res.status(201).json({
+            message: 'User created successfully',
+        });
     }
 }
