792f954ee6f16828a7d72dfd806b9f5d62086a79
Deepak Chauhan <imdeepakchauhan12@gmail.com>
2024-12-25 00:18:17 +0530

    Remove src/index.ts and update package-lock.json.
    Deleted the src/index.ts file as it was no longer needed. Updated the package-lock.json to reflect changes in dependencies or related configurations.

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -1,15 +1,18 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 import { User } from '../entity/User';
+import { Config } from './index';
 
 export const AppDataSource = new DataSource({
     type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'test',
-    password: 'test',
-    database: 'test',
-    synchronize: true,
+    host: Config.DB_HOST,
+    port: Number(Config.DB_PORT),
+    username: Config.DB_USERNAME,
+    password: Config.DB_PASSWORD,
+    database: Config.DB_NAME,
+    // should not be true in production
+    synchronize:
+        Config.NODE_ENV === 'development' || Config.NODE_ENV === 'test',
     logging: false,
     entities: [User],
     migrations: [],
--- src/config/index.ts
+++ src/config/index.ts
@@ -7,8 +7,18 @@ config({
 
 const PORT = process.env.PORT;
 const NODE_ENV = process.env.NODE_ENV;
+const DB_HOST = process.env.DB_HOST;
+const DB_PORT = process.env.DB_PORT;
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
 
 export const Config = Object.freeze({
     PORT,
     NODE_ENV,
+    DB_HOST,
+    DB_PORT,
+    DB_USERNAME,
+    DB_PASSWORD,
+    DB_NAME,
 });
